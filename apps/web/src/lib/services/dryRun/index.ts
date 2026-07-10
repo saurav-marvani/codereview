a0614e76bcd23efcb4b6ea76b14efb6debca296f
Junior Sartori <sartorijr92@gmail.com>
2026-07-10 09:01:01 -0300

    Removing dry run function in front-end

--- apps/web/src/lib/services/dryRun/index.ts
+++
@@ -1,9 +0,0 @@
-import { pathToApiUrl } from "src/core/utils/helpers";
-
-export const DRY_RUN_PATHS = {
-    EXECUTE: pathToApiUrl("/dry-run/execute"),
-    GET_STATUS: pathToApiUrl("/dry-run/status"),
-    SSE_EVENTS: pathToApiUrl("/dry-run/events"),
-    GET: pathToApiUrl("/dry-run"),
-    LIST: pathToApiUrl("/dry-run"),
-};
